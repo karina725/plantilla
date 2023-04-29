b14892986f34489c9de6b8bca59f25ebf91b4b3e
Karina <129091450+karina725@users.noreply.github.com>
2023-04-28 20:35:22 -0600

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,7 +32,7 @@ class Database extends Config
      */
     public $default = [
         'DSN'      => '',
-        'hostname' => 'cleardb-adjacent-75489',
+        'hostname' => 'us-mm-auto-sfo-06-bh.cleardb.net',
         'username' => 'b264e7f33d46d8',
         'password' => 'a4b22f42',
         'database' => 'heroku_63529e2d71924ab',
