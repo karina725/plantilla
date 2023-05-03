1507ce24cc44fc2bab9d8aa2b47a81212d812e98
Karina <129091450+karina725@users.noreply.github.com>
2023-05-03 12:39:11 -0600

    Delete app directory

--- app/Views/Gallery/all.php
+++
@@ -1,9 +0,0 @@
-<?php setlocale(LC_TIME, "esp"); ?>
-<div class="container">
-    <h2>Galería</h2>
-    <?php foreach ($posts as $item): ?>
-        <?php if((session()->user===$item['user']) || (session()->admin==1) ){ ?>
-            <img src="<?=$item['image'];?>" alt="<?=$item['image'];?>" width="300px" heigth="300px">
-        <?php } ?>
-    <?php endforeach; ?>
-</div>
